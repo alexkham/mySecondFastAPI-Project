5f59c0d28158dd45c8cf82ca8a86261bcfcd65cc
alexkham <alexkha@netvision.net.il>
2023-09-02 18:34:56 +0300

    second FastApi app

--- .venv/main.py
+++ .venv/main.py
@@ -1,1 +1,12 @@
 #Fast API is great
+from fastapi import FastAPI
+
+app=FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message":"HELLO"}
+
+@app.get("/question")
+async def question():
+    return {"message":"How old are you?"}
