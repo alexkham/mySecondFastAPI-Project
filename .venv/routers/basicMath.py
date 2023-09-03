2d9bfa5ad6af399a3c76f5fa7a97cad12a2fe523
alexkham <alexkha@netvision.net.il>
2023-09-03 14:38:50 +0300

    router added

--- .venv/routers/basicMath.py
+++ .venv/routers/basicMath.py
@@ -10,3 +10,8 @@ router=APIRouter(
 def add(x,y):
     result=int(x)+int(y)
     return {"result":str(result)}
+
+@router.get('/multiply')
+def add(x,y):
+    result=float(x)*float(y)
+    return {"result":str(result)}
